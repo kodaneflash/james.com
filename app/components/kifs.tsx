2f8d43e994b55a392bf7804c41b1e74f69865ccc
kodaneflash <jamesonmarcher@gmail.com>
2024-06-30 23:42:43 -0400

    j

--- app/components/kifs.tsx
+++ app/components/kifs.tsx
@@ -5,19 +5,13 @@ type CloudinaryVideoProps = Parameters<typeof CloudinaryVideo>[0]
 export function MissingSomething(
 	props: Omit<CloudinaryVideoProps, 'cloudinaryId'>,
 ) {
-	return (
-		<CloudinaryVideo cloudinaryId="imaginify/misc/where_am_i" {...props} />
-	)
+	return <CloudinaryVideo cloudinaryId="imaginify/misc/where_am_i" {...props} />
 }
 
 export function Grimmacing(props: Omit<CloudinaryVideoProps, 'cloudinaryId'>) {
-	return (
-		<CloudinaryVideo cloudinaryId="imaginify/misc/grimmace" {...props} />
-	)
+	return <CloudinaryVideo cloudinaryId="imaginify/misc/grimmace" {...props} />
 }
 
 export function Facepalm(props: Omit<CloudinaryVideoProps, 'cloudinaryId'>) {
-	return (
-		<CloudinaryVideo cloudinaryId="imaginify/misc/facepalm" {...props} />
-	)
+	return <CloudinaryVideo cloudinaryId="imaginify/misc/facepalm" {...props} />
 }
